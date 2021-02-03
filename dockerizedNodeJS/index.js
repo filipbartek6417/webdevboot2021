bdec0d537fa4baacf94147b132c7c6ec774924d2
filipbartek6417 <66736221+filipbartek6417@users.noreply.github.com>
2021-02-03 19:44:25 +0100

    dockerizedNodeJS

---
+++ dockerizedNodeJS/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express');
+const PORT = 8000;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello World!</h1>`);
+})
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
