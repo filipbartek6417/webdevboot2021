3327c11e3672c7d79cdd6e721e4a3049e8c7eb8b
filipbartek6417 <66736221+filipbartek6417@users.noreply.github.com>
2021-02-05 09:27:31 +0100

    comments

--- templating/index.js
+++ templating/index.js
@@ -1,11 +1,77 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const {v4: uuid} = require('uuid');
+const methodOverride = require('method-override');
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
+let comments = [
+  {
+    id: uuid(),
+    user: "alzbetka",
+    text: "where"
+  },{
+    id: uuid(),
+    user: "betka",
+    text: "is"
+  },{
+    id: uuid(),
+    user: "cibetka",
+    text: "my"
+  },{
+    id: uuid(),
+    user: "debetka",
+    text: "cocaine"
+  }
+]
+
 app.get("/", (req, res) => {
   res.render('home');
+})
+
+app.get("/comments", (req, res) => {
+  res.render('comments/index', {comments})
+})
+
+app.get("/comments/new", (req, res) => {
+  res.render('comments/new')
+})
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find(c => c.id === id);
+  res.render("comments/show", {comment})
+})
+
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find(c => c.id === id);
+  res.render("comments/edit", {comment});
+})
+
+app.patch("/comments/:id", (req, res) => {
+  const {id} = req.params;
+  const newText = req.body.text;
+  const foundComment = comments.find(c => c.id === id);
+  foundComment.text = newText;
+  res.redirect("/comments");
+})
+
+app.delete("/comments/:id", (req, res) => {
+  const {id} = req.params;
+  comments = comments.filter(c => c.id !== id);
+  res.redirect("/comments")
+})
+
+app.post("/comments", (req, res) => {
+  const {user, text} = req.body;
+  comments.push({ user, text, id: uuid() })
+  res.redirect("/comments");
 })
 
 app.get("/r/:sub", (req,res) => {
