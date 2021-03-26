7eea62f6df2156ba555f5459c66a889940123b02
Filip <66736221+filipbartek6417@users.noreply.github.com>
2021-03-26 19:54:33 +0100

    yelpcamp2021 pt.3 - basic styling

--- yelpcamp2021/app.js
+++ yelpcamp2021/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const engine = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 mongoose.connect("mongodb://localhost:27017/yelpcamp", {
@@ -13,6 +14,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", () => console.log("Database connected"));
 
+app.engine('ejs', engine)
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}))
