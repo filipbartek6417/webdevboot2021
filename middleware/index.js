a498d5310153080aaa42d89201a46de27af76e07
Filip <66736221+filipbartek6417@users.noreply.github.com>
2021-03-27 09:48:13 +0100

    error handling

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./AppError');
 const app = express();
 
 app.use(morgan('dev'));
@@ -13,7 +14,7 @@ const verify = (req, res, next) => {
   if (password === 'chickennugget') {
     next();
   }
-  res.send('Need a password')
+  throw new AppError('password required!', 418);
 }
 
 app.get("/", (req, res) => {
@@ -25,12 +26,21 @@ app.get("/dogs", (req, res) => {
   res.send('Woof');
 })
 
+app.get("/error", (req, res) => {
+  chicken.fly();
+})
+
 app.get("/secret", verify, (req, res) => {
   res.send("you are here yay");
 })
 
 app.use((req, res) => {
   res.status(404).send("We do not do that here");
+})
+
+app.use((err, req, res, next) => {
+  const { status = 418, message = "POZOR ERROR!" } = err
+  res.status(status).send(message);
 })
 
 app.listen(3000, () => console.log("Running on port 3000"));
